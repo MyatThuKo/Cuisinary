3288372ed9065350f79fa8adfb7f061a03976af6
Myat Thu Ko (Tony) <nymyatthuko@gmail.com>
2024-12-13 16:24:51 -0500

    Initial Commit

---
+++ Cuisinary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cuisinary
+//
+//  Created by Myat Thu Ko on 12/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cuisinary/CuisinaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuisinaryApp.swift
+//  Cuisinary
+//
+//  Created by Myat Thu Ko on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CuisinaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
